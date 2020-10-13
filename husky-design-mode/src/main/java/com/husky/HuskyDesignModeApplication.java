b1788ab5d8d3d339b058a9224743c60b6e87d6be
liuph <liuph@ckt.cn>
2020-10-13 19:24:56 +0800

    设计模式项目

---
+++ husky-design-mode/src/main/java/com/husky/HuskyDesignModeApplication.java
@@ -0,0 +1,13 @@
+package com.husky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyDesignModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskyDesignModeApplication.class, args);
+	}
+
+}
