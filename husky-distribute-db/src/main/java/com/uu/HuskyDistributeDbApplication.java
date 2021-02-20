d1fdbaa2b7f261dccd66df19ae4b4ff6b306a155
haopengge <anhusky@163.com>
2021-02-20 17:52:48 +0800

    【feat】移除

---
+++ husky-distribute-db/src/main/java/com/uu/HuskyDistributeDbApplication.java
@@ -0,0 +1,13 @@
+package com.uu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyDistributeDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuskyDistributeDbApplication.class, args);
+    }
+
+}
