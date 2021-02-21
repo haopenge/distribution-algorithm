88d7ad1073c311a009f529dac4a733c814f1371b
nbdoc <anhusky@163.com>
2021-02-21 19:44:26 +0800

    【feat】Nio Server

--- husky-netty/src/main/java/com/husky/demo/source/EchoServer.java
+++ husky-netty/src/main/java/com/husky/demo/source/EchoServer.java
@@ -1,34 +1,62 @@
 package com.husky.demo.source;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.FixedLengthFrameDecoder;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
 
 /**
- * @desc 响应式服务器
  * @author liuph
+ * @desc 响应式服务器
  * @date 2021/01/20 10:32
  */
 public class EchoServer {
-    public void startEchoServer(int port){
+    public static void startEchoServer(int port) throws InterruptedException {
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup work = new NioEventLoopGroup();
 
-        ServerBootstrap bootstrap = new ServerBootstrap().group(boss, work)
+        ServerBootstrap b = new ServerBootstrap().group(boss, work)
                 .channel(NioServerSocketChannel.class)
-                .handler(new LoggingHandler(LogLevel.INFO)) // 设置ServerSocketChannel 对应的 Handler
+                .handler(new SimpleServerHandler())
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new FixedLengthFrameDecoder(10));
-                             //   .addLast(new ResponseSampleEncoder())
-                             //   .addLast(new RequestSampleHandler());
+
                     }
                 });
+        try {
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
     }
+
+    private static class SimpleServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("channelActive");
+        }
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("channelRegistered");
+        }
+
+        @Override
+        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("handlerAdded");
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+       startEchoServer(80);
+    }
+
 }
