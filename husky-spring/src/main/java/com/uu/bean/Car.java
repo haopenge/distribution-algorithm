7825371738d3a66f578ad0cbe0eb4020063f9cbb
haopengge <anhusky@163.com>
2021-02-24 11:53:30 +0800

    【feat】移除分库分表项目

--- husky-spring/src/main/java/com/uu/bean/Car.java
+++ husky-spring/src/main/java/com/uu/bean/Car.java
@@ -1,6 +1,12 @@
 package com.uu.bean;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationStartupAware;
+import org.springframework.core.metrics.ApplicationStartup;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,10 +15,28 @@ import org.springframework.stereotype.Component;
  * @date 2021/02/20 19:16
  */
 @Component
-public class Car implements BeanNameAware {
+public class Car implements ApplicationStartupAware, ApplicationContextAware,BeanNameAware , BeanPostProcessor {
+
+    private ApplicationContext applicationContext;
+
+    private String beanName;
+
+    private ApplicationStartup applicationStartup;
 
     @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(" --------------------->    3");
+        System.out.println(applicationContext.toString());
+    }
+    @Override
     public void setBeanName(String s) {
+        System.out.println(" --------------------->    1");
         s =  "car";
+    }
+
+    @Override
+    public void setApplicationStartup(ApplicationStartup applicationStartup) {
+        System.out.println(" --------------------->    2");
+        System.out.println(applicationStartup.toString());
     }
 }
